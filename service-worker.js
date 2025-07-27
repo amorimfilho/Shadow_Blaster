7c3ddc31acb4967987695845578047326727a20e
amorimfilho <143339963+amorimfilho@users.noreply.github.com>
2025-07-27 16:58:55 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('blaster-store').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log("Service Worker instalado.");
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  console.log("Service Worker ativado.");
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
